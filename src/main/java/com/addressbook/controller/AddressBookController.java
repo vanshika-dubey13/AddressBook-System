e94d0b245fede95019f22f20af5476212933448a
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-03-09 15:58:47 +0530

    [Vanshika] : added UC-22 task

---
+++ src/main/java/com/addressbook/controller/AddressBookController.java
@@ -0,0 +1,47 @@
+package com.addressbook.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.addressbook.model.Person;
+import com.addressbook.service.AddressBook;
+
+@RestController
+@RequestMapping("/contacts")
+public class AddressBookController {
+
+    private AddressBook addressBook = new AddressBook();
+
+    // Constructor
+    public AddressBookController() {
+
+        addressBook.addContact(
+            new Person("Vikash","Prajapati","Bhopal","Bhopal","MP","462001","9999999999","vikash@gmail.com")
+        );
+
+        addressBook.addContact(
+            new Person("Rahul","Sharma","Indore","Indore","MP","452001","8888888888","rahul@gmail.com")
+        );
+    }
+
+    // Get all contacts
+    @GetMapping
+    public List<Person> getContacts() {
+
+        return addressBook.getContacts();
+    }
+    
+    // Add new contact
+    @PostMapping
+    public String addContact(@RequestBody Person person) {
+
+        addressBook.addContact(person);
+
+        return "Contact added successfully";
+    }
+}
